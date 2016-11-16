49fb058bbbbe12cdd8847bf2e5e4d7afa6b9ca7b
Long Yang <longyang_123@yeah.net>
2016-11-16 11:33:22 +0800

    Update GameData

--- src/gomoku/GameData.java
+++ src/gomoku/GameData.java
@@ -3,7 +3,12 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * Record every play into file
@@ -13,21 +18,39 @@ import java.io.IOException;
 public class GameData {
 	private List<int[][]> list;
 	private int result;
+	private String name;
+	
+	/**
+	 * Default generation of Game Data
+	 */
 	public GameData(){
-		list = new ArrayList<int[][]>();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		this.name = df.format(new Date());
+		this.list = new ArrayList<int[][]>();
 	}
-	public GameData(List<int[][]> list){
+
+	/**
+	 * Generation of Game Data
+	 * @param name Game Data Name
+	 * @param list Data List
+	 */
+	public GameData(String name, List<int[][]> list){
+		this.name = name;
 		this.list = list;
 	}
+	
 	public void append(ChessBoard chessboard){
 		list.add(chessboard.ChessBoardInfo());
 	}
+	
 	public void GameEnd(int status){
 		this.result = status;
 	}
+	
 	public List<int[][]> getdata(){
 		return this.list;
 	}
+	
 	public int getresult(){
 		return this.result;
 	}
@@ -38,8 +61,14 @@ public class GameData {
 	 * @return GameData Game Data
 	 * @throws IOException
 	 */
-	public GameData load(String filename) throws IOException{
-		
+	public static GameData load(String filename) throws IOException{
+		FileReader fr = null;
+		BufferedReader br = null;
+		fr = new FileReader("./data/"+filename);
+		br = new BufferedReader(fr,100);
+		// TODO Read Data
+		br.close();
+		fr.close();
 		return new GameData();
 	}
 	
